54f4f410f37f4e581787a6f65d0af1c7b38b28f8
Aishwarya Chandrashekhar <achandrashekhar@usfca.edu>
2017-10-25 22:38:58 -0700

    displaying day of the month

--- src/CalendarMonth.js
+++ src/CalendarMonth.js
@@ -6,6 +6,7 @@ import CalendarDay from './CalendarDay';
 
 const weeks = [1, 2,3,4]
 const daysOfWeek  = [1,2,3,4,5,6,7]
+const daysofMonth = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]
 
 // this.props.totalAngle = the size of the slice of the pie
 
@@ -33,8 +34,9 @@ class CalendarMonth extends React.Component {
     const myslice =this.props.totalAngle / weeks.length
     console.log(`MYSLICE = ${myslice}`)
 
-    const numdays = weeks.map(w => {
+    const numdays = weeks.map((w,midx) => {
       const rot = this.props.rotation + (myslice * w)  //30+(30*1)
+      let dayNumber = 0;
       return(
         <Group>
         { daysOfWeek.map((d, idx) => {
@@ -44,9 +46,10 @@ class CalendarMonth extends React.Component {
           return <CalendarDay height={this.props.height}
           width={this.props.width}
           weeks={weeks.length}
-          dayNum={w*d}
+          dayNum={d+(7*(w-1))}
           numMonths={this.props.numMonths}
           key={`${d}${w}${this.props.month}`} rotation={rot} innerRadius={inner} outerRadius={inner+25} color={this.props.color} />})
+
         }
 
         </Group>
