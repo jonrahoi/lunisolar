97dfc33b50ef6aaff4226ddec298a1c1fa3ea9a9
Aishwarya Chandrashekhar <achandrashekhar@usfca.edu>
2017-10-29 12:52:43 -0700

    click anywhere on the blue square

--- src/YearToggle.js
+++ src/YearToggle.js
@@ -44,10 +44,10 @@ class YearToggle extends React.Component {
     return(
       <Group>
       <Rect width = {30} height = {30} x = {this.props.width/2-40} y={this.props.height/2-10} fill={"blue"} onClick = {this.mouseClickBackward.bind(this)}/>
-      <Arrow x={this.props.width/2-30} y={this.props.height/2+5} fill = {"white"} rotation = {180}  />
+      <Arrow x={this.props.width/2-30} y={this.props.height/2+5} fill = {"white"} rotation = {180} onClick = {this.mouseClickBackward.bind(this)} />
       <Text x = {this.props.width/2} y = {this.props.height/2} text={this.state.year}/>
       <Rect width = {30} height = {30} x = {this.props.width/2+40} y={this.props.height/2-10} fill={"blue"} onClick = {this.mouseClickForward.bind(this)}/>
-      <Arrow x={this.props.width/2+60} y={this.props.height/2+5} fill = {"white"} />
+      <Arrow x={this.props.width/2+60} y={this.props.height/2+5} fill = {"white"}  onClick = {this.mouseClickForward.bind(this)}/>
       </Group>
     )
   }
