72f40bb31102500bf68867cde3721aaece3ace3b
Jon Rahoi <jon.rahoi@creditkarma.com>
2018-01-10 18:44:24 -0800

    adding a generic test runner

--- src/test/unit.js
+++ src/test/unit.js
@@ -4,18 +4,30 @@ const expect = Code.expect;
 const moment = require('moment')
 const fs = require('fs');
 const {getAllHolidaysForYear, holidays} = require('../holidays')
+const testFolder = './testData/'
 
-it('holiday test', () => {
-  fs.readFile('./src/test/testData/spirit.txt', 'utf8', function (err, data) {
-    let dataarray = data.split('\n');
-    dataarray.forEach(d=>{
-      if(d!==""){
-        const dateFromInternet = moment(d, 'YYYY MMM D')
-        const holidayDate = holidays['Ghost Festival'](dateFromInternet.year()).date
-        const dateFromCode = moment(`${holidayDate.month}-${holidayDate.day}-${holidayDate.year}`, 'M-D-YYYY')
-        console.log(`${dateFromCode}  ::::  ${dateFromInternet}`);
-        expect(dateFromInternet.isSame(dateFromCode, 'day')).to.equal(true)
-      }
-    })
-  })
+const mapping = {
+  "spirit.txt" : 'Ghost Festival'
+}
+
+fs.readdirSync(testFolder).forEach(file => {
+    if (file.indexOf('.txt') !== -1){
+        const data = fs.readFileSync(`${testFolder}${file}`).toString()
+        it (`holiday test: ${file}`, () => {
+          let dataarray = data.split('\n');
+          const memberName = mapping[file]
+
+          dataarray.forEach(d=>{
+            if (d !== ''){
+              const dateFromInternet = moment(d, 'YYYY MMM D')
+              const holidayDate = holidays[memberName](dateFromInternet.year()).date
+              const dateFromCode = moment(`${holidayDate.month}-${holidayDate.day}-${holidayDate.year}`, 'M-D-YYYY')
+              console.log(`${dateFromCode}  ::::  ${dateFromInternet}`);
+              expect(dateFromInternet.isSame(dateFromCode, 'day')).to.equal(true)
+              done()
+            }
+          })
+
+        })
+    }
 })
